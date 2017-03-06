b8591a4204fdbc0d98e787fb1065c6a5252167a8
enesozturk <oztrkeness@gmail.com>
2017-03-06 09:46:17 +0300

    obajushopping

---
+++ ObajuShopping/Controllers/AboutUsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ObajuShopping.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.ActionName = "About Us";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.ActionName = "Contact";
+            return View();
+        }
+
+        public ActionResult FAQ()
+        {
+            ViewBag.ActionName = "Frequendly Asked Questions";
+            return View();
+        }
+    }
+}
