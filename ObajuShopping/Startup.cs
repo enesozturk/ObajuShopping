3be6db2baf10930b87c973e4a42a9eaba3945b23
enesozturk <oztrkeness@gmail.com>
2017-03-06 09:58:32 +0300

    update

---
+++ ObajuShopping/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ObajuShopping.Startup))]
+namespace ObajuShopping
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
