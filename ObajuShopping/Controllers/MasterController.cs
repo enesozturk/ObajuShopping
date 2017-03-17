b0e4342691032f259f5bb90adb20e8c861c4347c
enesozturk <oztrkeness@gmail.com>
2017-03-17 17:20:40 +0300

    Add tables to Navigation bar from database

--- ObajuShopping/Controllers/MasterController.cs
+++ ObajuShopping/Controllers/MasterController.cs
@@ -4,19 +4,28 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ObajuShopping.Models;
+using ObajuShopping.ViewModels;
 
 namespace ObajuShopping.Controllers
 {
     public class MasterController : Controller
     {
         AaadbEntities db = new AaadbEntities();
-        public List<Category> menClothing()
+        
+        public CategoryViewModel NavbarCategories()
         {
-            return db.Categories.Where(p => p.topCategory == 2).ToList();
+            CategoryViewModel cvm = new CategoryViewModel();
+            cvm.menClothing = db.Categories.Where(p => p.topCategory == 4).ToList();
+            cvm.menShoes = db.Categories.Where(p => p.topCategory == 5).ToList();
+            cvm.menAccessories = db.Categories.Where(p => p.topCategory == 6).ToList();
+            cvm.LadyClothing = db.Categories.Where(p => p.topCategory == 16).ToList();
+            cvm.LadyShoes = db.Categories.Where(p => p.topCategory == 17).ToList();
+            cvm.LadyAccessories = db.Categories.Where(p => p.topCategory == 18).ToList();
+            return cvm;
         }
         public PartialViewResult _navbarCategories()
         {
-            var model = menClothing();
+            var model = NavbarCategories();
             return PartialView(model);
         }
     }
---
+++ ObajuShopping/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ObajuShopping.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ObajuShopping.ViewModels
+{
+    public class CategoryViewModel
+    {
+        public List<Category> menClothing;
+        public List<Category> menShoes;
+        public List<Category> menAccessories;
+        public List<Category> LadyClothing;
+        public List<Category> LadyShoes;
+        public List<Category> LadyAccessories;
+    }
+}
