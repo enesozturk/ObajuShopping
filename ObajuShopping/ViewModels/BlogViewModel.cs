ea9a668330310bd10595ce172187fb59b0392033
enesozturk <oztrkeness@gmail.com>
2017-03-17 18:26:27 +0300

    Add missing Viewmodels and classes..

---
+++ ObajuShopping/ViewModels/BlogViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ObajuShopping.Models;
+
+namespace ObajuShopping.ViewModels
+{
+    public class BlogViewModel
+    {
+        public Blog post { get; set; }
+        public List<Blog> LatestPosts { get; set; }
+    }
+}
---
+++ ObajuShopping/ViewModels/HomeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ObajuShopping.Models;
+
+namespace ObajuShopping.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<Blog> Posts { get; set; }
+        public List<Product> LatestProducts { get; set; }
+        public List<Product> FeaturedProducts { get; set; }
+        public List<Slider> Sliders { get; set; }
+    }
+}
