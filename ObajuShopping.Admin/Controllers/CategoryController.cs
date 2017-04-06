2baa1920f9b1a24032a898c8a855e41fce6b1935
enesozturk <enesozturk.dev@gmail.com>
2017-04-06 16:25:49 +0300

    Add Dashboad Page, Category and Product Controller

---
+++ ObajuShopping.Admin/Controllers/CategoryController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ObajuShopping.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public ActionResult List()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+        public ActionResult UpdateProduct()
+        {
+            return View();
+        }
+    }
+}
---
+++ ObajuShopping.Admin/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+
+namespace ObajuShopping.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult List()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+        public ActionResult UpdateProduct()
+        {
+            return View();
+        }
+    }
+}
