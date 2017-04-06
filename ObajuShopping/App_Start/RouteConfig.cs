f90c3f74f75b493ee1848d830375540ac78d975b
enesozturk <enesozturk.dev@gmail.com>
2017-04-06 13:24:35 +0300

    Add ObajuShopping Admin Panel Project

--- ObajuShopping/App_Start/RouteConfig.cs
+++ ObajuShopping/App_Start/RouteConfig.cs
@@ -13,11 +13,19 @@ namespace ObajuShopping
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute(
+                name: "AdminDefault",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "ObajuShopping.Admin.Controller" }
+                );
+
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "ObajuShopping.Controllers" }
             );
 
 
